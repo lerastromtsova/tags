f8713a86edd40f672aa0b0fb9a18afaab22c5a03
lerastromtsova <v.stromtsova@yandex.ru>
2017-06-15 18:55:55 +0300

    Это просто коммит, чтобы тут ошибка не вылезала

--- views.py
+++ views.py
@@ -5,27 +5,18 @@ from django.http import HttpResponseRedirect
 from .forms import SearchForm
 
 def search(request):
-    if 'tag' in request.GET and request.GET['tag']!='':
-        return HttpResponseRedirect('/search/')
+    if request.GET['tag'] != '':
+        return render(request, 'search_results.html', {'tag': request.GET['tag']})
     else:
         return HttpResponse('You submitted an empty form.')
 
 
-
-
 def search_form(request):
-    # if this is a POST request we need to process the form data
     if request.method == 'GET':
-        # create a form instance and populate it with data from the request:
         form = SearchForm(request.GET)
-        # check whether it's valid:
+        tagname = request.GET['tag']
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/search/')
-
-    # if a GET (or any other method) we'll create a blank form
+            return HttpResponseRedirect('/search/?tag=%tagname/')
     else:
         form = SearchForm()
 
